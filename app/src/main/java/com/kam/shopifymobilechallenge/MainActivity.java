85e4cdca94745b43b9f6dab62e9265706a8c08ea
Kam <kam@kamrant.com>
2019-01-20 01:34:53 -0500

    Working interface

--- app/src/main/java/com/kam/shopifymobilechallenge/MainActivity.java
+++ app/src/main/java/com/kam/shopifymobilechallenge/MainActivity.java
@@ -2,12 +2,51 @@ package com.kam.shopifymobilechallenge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.*;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "MainActivity";
+    private final String url = "https://shopicruit.myshopify.com/admin/custom_collections.json?page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6";
+    OkHttpClient client = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        client =  new OkHttpClient();
+        final Button relButton = (Button)findViewById(R.id.relButton);
+        relButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "Kam", Toast.LENGTH_LONG).show();
+                showData();
+            }
+        });
     }
+
+
+
+    public void showData() throws IOException{
+        Log.d("Something",run(url));
+    }
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
 }
